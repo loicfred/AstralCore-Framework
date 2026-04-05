302244e1224b820cc8f858c90188350ac4de4b0b
Loïc Fred <loic.cheerkoot@gmail.com>
2026-04-06 00:58:56 +0400

    Fix pom.xml

--- modules/AI/src/main/java/org/astralcore/ai/spring/Conversation.java
+++ modules/AI/src/main/java/org/astralcore/ai/spring/Conversation.java
@@ -15,10 +15,10 @@ import static org.astralcore.ai.spring.AIService.aiService;
 public class Conversation {
 
     private final List<Message> msgs = new java.util.ArrayList<>();
-    private Object[] toolsObjects;
+    private List<Object> toolsObjects = new java.util.ArrayList<>();
 
     protected Conversation(String myName, SystemMessage systemMessage, Object... toolsObjects) {
-        this.toolsObjects = toolsObjects;
+        this.toolsObjects.addAll(List.of(toolsObjects));
         msgs.add(systemMessage != null ? systemMessage : SystemMessage.builder().text("""
                 You are an AI model designed to answer have a conversation with %s.
                 Keep answers clear and short paragraphs, avoid switching lines too much.
@@ -26,19 +26,31 @@ public class Conversation {
                 """.formatted(myName != null ? myName : "a user")).build());
     }
 
-    public String talk(String yourMessage) {
+    public String talk(String yourMessage, Object... toolsObj) {
+        addToTools(toolsObj);
         return chat(yourMessage, toolsObjects, aiService.Conversationist);
     }
-    public String askForInformation(String yourMessage) {
+    public String askForInformation(String yourMessage, Object... toolsObj) {
+        addToTools(toolsObj);
+        toolsObjects = new java.util.ArrayList<>(new java.util.LinkedHashSet<>(toolsObjects));
         return chat(yourMessage, toolsObjects, aiService.Informationist);
     }
-    public String askToChooseBetweenItems(String yourMessage) {
+    public String askToChooseBetweenItems(String yourMessage, Object... toolsObj) {
+        addToTools(toolsObj);
         return chat(yourMessage, toolsObjects, aiService.ItemChooser);
     }
 
-    private @Nullable String chat(String yourMessage, Object[] toolsObjects, ChatOptions options) {
+    private void addToTools(Object[] toolsObj) {
+        for (Object obj : toolsObj) {
+            if (toolsObjects.stream().noneMatch(o -> o.getClass().equals(obj.getClass()))) {
+                toolsObjects.add(obj);
+            }
+        }
+    }
+
+    private @Nullable String chat(String yourMessage, List<Object> toolsObjects, ChatOptions options) {
         msgs.add(UserMessage.builder().metadata(Map.of("timestamp", System.currentTimeMillis())).text(yourMessage).build());
-        msgs.add(aiService.doPrompt(Prompt.builder().messages(msgs).chatOptions(options).build(), toolsObjects));
+        msgs.add(aiService.doPrompt(Prompt.builder().messages(msgs).chatOptions(options).build(), toolsObjects.toArray()));
         return msgs.getLast().getText();
     }
 
